2a9e581eb4586f5e5382144ab7546807d1960694
haiturtle <haiturtle@aol.com>
2022-05-28 18:51:49 -0400

    ADD: the shell of the other routes

--- index.js
+++ index.js
@@ -6,9 +6,17 @@ const PORT = 4444
 const app = new App()
 
 app
-  .get('/videos/:videoId.mp4/', (req, res) => {
+  .get('/videos/:videoId.mp4/group-of-pictures.json', (req, res) => {
     const { videoId } = req.params
     res.send({ message: `Should get ${videoId} group of pictures`})
+
+  })
+  .get('/videos/:videoId.mp4/group-of-pictures/:groupIndex.mp4', (req, res) => {
+    res.send({ message: `implement me`})
+  })
+  .get('/videos/:videoId.mp4/group-of-pictures', (req, res) => {
+    const { videoId } = req.params
+    res.send({ message: `implement me 2`})
 
   })
 .listen(PORT, () => {
